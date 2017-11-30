905a1101c8700642f7ad568239bfc100296e5e54
Phivos Stylianides <stphivos@gmail.com>
2017-11-30 22:28:25 +0200

    Update version to v1.0.4

--- setup.py
+++ setup.py
@@ -4,15 +4,22 @@ from pip.req import parse_requirements
 install_req = parse_requirements('requirements/core.txt', session='skip')
 req = [str(ir.req) for ir in install_req]
 
+
+def read_md(filename):
+    from pypandoc import convert_file
+    return convert_file(filename, 'rst')
+
+
 setup(
     name='django_mock_queries',
     packages=['django_mock_queries'],
-    version='1.0.2',
+    version='1.0.4',
     description='A django library for mocking queryset functions in memory for testing',
+    long_description=read_md('README.md'),
     author='Phivos Stylianides',
     author_email='stphivos@gmail.com',
     url='https://github.com/stphivos/django-mock-queries',
-    keywords=['django', 'mocking', 'unit-testing', 'tdd'],
+    keywords='django orm mocking unit-testing tdd',
     classifiers=[],
     install_requires=req
 )
