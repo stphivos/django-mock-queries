740574e9afbe8d05b0cc51659ceb46ec2af36c88
Phivos Stylianides <stphivos@gmail.com>
2018-08-09 11:40:19 +0300

    Update version to v2.1.0

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
+
 try:
     from pip._internal.req import parse_requirements
 except ImportError:
     from pip.req import parse_requirements
-
 
 install_req = parse_requirements('requirements/core.txt', session='skip')
 req = [str(ir.req) for ir in install_req]
@@ -19,14 +19,30 @@ def read_md(filename):
 
 setup(
     name='django_mock_queries',
-    packages=['django_mock_queries'],
-    version='2.0.1',
+    version='2.1.0',
     description='A django library for mocking queryset functions in memory for testing',
     long_description=read_md('README.md'),
+    url='https://github.com/stphivos/django-mock-queries',
     author='Phivos Stylianides',
     author_email='stphivos@gmail.com',
-    url='https://github.com/stphivos/django-mock-queries',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Testing :: Mocking',
+        'Topic :: Software Development :: Testing :: Unit',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
     keywords='django orm mocking unit-testing tdd',
-    classifiers=[],
-    install_requires=req
+    packages=['django_mock_queries'],
+    install_requires=req,
 )
