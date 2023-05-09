d656806b5a508b4773cd5fbd83ca53b21c027b68
stefan6419846 <96178532+stefan6419846@users.noreply.github.com>
2023-05-09 09:56:23 +0200

    support Django 3.2 and Django 4 as well

--- examples/users/users/urls.py
+++ examples/users/users/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+try:
+    from django.urls import re_path
+except ImportError:
+    # Django 1.11
+    from django.conf.urls import url as re_path
 
 from analytics import views
 
 urlpatterns = [
-    url(r'users/active', views.active_users),
+    re_path(r'users/active', views.active_users),
 ]
