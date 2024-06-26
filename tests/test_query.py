0435ca3038ddce643a58b9865df98c8432bbf7af
dvirh <dvir.h@qspark.co>
2024-06-26 16:17:03 +0300

    added missing self argument

--- tests/test_query.py
+++ tests/test_query.py
@@ -1357,7 +1357,7 @@ class TestQuery(TestCase):
         self.assertEqual(mockset.count(), 2)
         self.assertEqual(mockset.filter(Q()).count(), 2)
     
-    def test_mock_model_annotation_by_nested_field():
+    def test_mock_model_annotation_by_nested_field(self):
         mockset = MockSet(
             MockModel(id=1, nested_mock=MockModel(id=1, field1="field_value"))
         )
